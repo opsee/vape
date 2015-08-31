4b0c0985fd779582cbe151034fa45cb473090490
Greg Poirier <grep@binary-snobbery.com>
2015-08-31 10:04:41 -0700

    Add error messages to main

--- src/cmd/vape/main.go
+++ src/cmd/vape/main.go
@@ -10,17 +10,36 @@ import (
 )
 
 func main() {
-	key, err := ioutil.ReadFile(os.Getenv("VAPE_KEYFILE"))
+	keyPath := os.Getenv("VAPE_KEYFILE")
+	if keyPath == "" {
+		log.Fatal("Must set VAPE_KEYFILE environment variable.")
+	}
+	key, err := ioutil.ReadFile(keyPath)
 	if err != nil {
+		log.Println("Unable to read keyfile:", keyPath)
 		log.Fatal(err)
 	}
 	token.Init(key)
 
-	err = store.Init(os.Getenv("POSTGRES_CONN"))
+	pgConn := os.Getenv("POSTGRES_CONN")
+	if pgConn == "" {
+		log.Fatal("Must set POSTGRES_CONN environment variable.")
+	}
+	err = store.Init(pgConn)
 	if err != nil {
+		log.Println("Unable to open postgres store:", pgConn)
 		log.Fatal(err)
 	}
 
 	api.InjectLogger(os.Stdout)
+	publicHost := os.Getenv("VAPE_PUBLIC_HOST")
+	privateHost := os.Getenv("VAPE_PRIVATE_HOST")
+	if publicHost == "" {
+		log.Fatal("Must set VAPE_PUBLIC_HOST environment variable.")
+	}
+	if privateHost == "" {
+		log.Fatal("Must set VAPE_PRIVATE_HOST environment variable.")
+	}
+
 	api.ListenAndServe(os.Getenv("VAPE_PUBLIC_HOST"), os.Getenv("VAPE_PRIVATE_HOST"))
 }
