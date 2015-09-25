caeec607d194d917c95ddb0b04ef2f0a61cb698b
Mark Martin <assplecake@gmail.com>
2015-09-25 09:19:56 -0700

    add the email in the tokin

--- src/github.com/opsee/vape/model/user.go
+++ src/github.com/opsee/vape/model/user.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	Id           int       `json:"id" token:"id"`
 	CustomerId   string    `json:"customer_id" token:"customer_id" db:"customer_id"`
-	Email        string    `json:"email"`
+	Email        string    `json:"email" token:"email"`
 	Name         string    `json:"name"`
 	Verified     bool      `json:"verified" token:"verified"`
 	Admin        bool      `json:"admin" token:"admin"`
