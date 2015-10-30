0c5e2dcfffa76a5549367b0a262d5e22cada7261
Mark Martin <assplecake@gmail.com>
2015-10-30 14:21:59 -0700

    pepe

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@ APPENV=${APPENV:-vapenv}
 # relying on set -e to catch errors?
 /opt/bin/ec2-env > /ec2env
 eval "$(< /ec2env)"
-/opt/bin/s3kms get -b opsee-keys -o dev/$APPENV > /$APPENV
-/opt/bin/s3kms get -b opsee-keys -o dev/vape.key > /vape.key
+/opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
+/opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/vape.key > /vape.key
 
 # these will have to wait
 # TODO: tls from load-balancer -> vape
-# /opt/bin/s3kms get -b opsee-keys -o dev/vape-cert.pem > /vape-cert.pem
-# /opt/bin/s3kms get -b opsee-keys -o dev/vape-key.pem > /vape-key.pem
+# /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/vape-cert.pem > /vape-cert.pem
+# /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/vape-key.pem > /vape-key.pem
 
 source /$APPENV && \
 	/opt/bin/migrate -url "$POSTGRES_CONN" -path /migrations up && \
