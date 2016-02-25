7b8fa305bfa8bcfe0aa32afbec038f8c17f8a0f0
Mark Martin <assplecake@gmail.com>
2016-02-25 12:49:45 -0800

    download the right cert

--- run.sh
+++ run.sh
@@ -4,13 +4,11 @@ set -e
 APPENV=${APPENV:-vapenv}
 
 /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
-/opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/vape.key > /vape.key
-
-/opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$VAPE_CERT > /$VAPE_CERT
-/opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$VAPE_CERT_KEY > /$VAPE_CERT_KEY
-
-chmod 600 /$VAPE_CERT_KEY
 
 source /$APPENV && \
+  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/vape.key > /vape.key && \
+  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$VAPE_CERT > /$VAPE_CERT && \
+  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$VAPE_CERT_KEY > /$VAPE_CERT_KEY && \
+  chmod 600 /$VAPE_CERT_KEY && \
 	/opt/bin/migrate -url "$POSTGRES_CONN" -path /migrations up && \
 	/vape
