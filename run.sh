2302179e5d717129a7a1f1d2a802dbd3f23ab295
Mark Martin <assplecake@gmail.com>
2016-07-08 15:28:54 -0700

    don't migrate

--- run.sh
+++ run.sh
@@ -10,5 +10,4 @@ source /$APPENV && \
   /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$VAPE_CERT > /$VAPE_CERT && \
   /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$VAPE_CERT_KEY > /$VAPE_CERT_KEY && \
   chmod 600 /$VAPE_CERT_KEY && \
-	/opt/bin/migrate -url "$POSTGRES_CONN" -path /migrations up && \
 	/vape
